3d74834003c15e599c1045f98978606aa26d444c
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-06-09 20:26:41 +0530

    db authentication

--- jobportal-backend/src/main/java/com/eazybytes/jobportal/security/JobPortalSecurityConfig.java
+++ jobportal-backend/src/main/java/com/eazybytes/jobportal/security/JobPortalSecurityConfig.java
@@ -6,15 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -34,7 +30,8 @@ public class JobPortalSecurityConfig {
     private final List<String> securedPaths;
 
     @Bean
-    SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain customSecurityFilterChain(HttpSecurity http)
+            throws Exception {
 
         return http
                 .csrf(csrf -> csrf.disable())
@@ -54,40 +51,19 @@ public class JobPortalSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-
-        var user1 = User.builder()
-                .username("madan")
-                .password("$2a$10$viS6XrG2FpiZXPQgP7.rQeBrG6TauRaybxsaNjNi.WCLCdIURzZCq")
-                .roles("USER")
-                .build();
-
-        var user2 = User.builder()
-                .username("admin")
-                .password("$2a$10$CurDmUEPRQsX5AhEN1NSV.ejUteU0S3dj5XfjxOjaVFhCCTOuj8WG")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager() {
-
-        var authenticationProvider =
-                new DaoAuthenticationProvider(userDetailsService());
-
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-
-        return new ProviderManager(authenticationProvider);
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
-    public CompromisedPasswordChecker CompromisedPasswordChecker(){
+    public AuthenticationManager authenticationManager(
+            JobPortalUsernamePwdAuthenticationProvider provider) {
+
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
         return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
