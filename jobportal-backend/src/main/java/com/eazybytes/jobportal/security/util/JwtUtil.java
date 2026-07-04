57e272bef4d06f537761b00ff817775ec98df72b
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-07-05 01:24:24 +0530

    config values

--- jobportal-backend/src/main/java/com/eazybytes/jobportal/security/util/JwtUtil.java
+++ jobportal-backend/src/main/java/com/eazybytes/jobportal/security/util/JwtUtil.java
@@ -21,8 +21,10 @@ public class JwtUtil {
 
     private final Environment env;
 
+
     public String generateJwtToken(Authentication authentication){
         String jwtToken;
+        String ttlTime = env.getProperty("cache.jobs.ttl-minutes","5");
         String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
                 ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
         SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
