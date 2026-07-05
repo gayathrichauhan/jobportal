c3eb10c422100f18b3a4ceea63411def55ae68f5
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-07-05 22:59:49 +0530

    loadingg custom properties

--- jobportal-backend/src/main/java/com/eazybytes/jobportal/security/JobPortalSecurityConfig.java
+++ jobportal-backend/src/main/java/com/eazybytes/jobportal/security/JobPortalSecurityConfig.java
@@ -1,23 +1,33 @@
 package com.eazybytes.jobportal.security;
 
+import com.eazybytes.jobportal.audit.AuditorAwareImpl;
 import com.eazybytes.jobportal.security.filter.JwtTokenValidatorFilter;
+import com.eazybytes.jobportal.security.util.CorsProperties;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -25,6 +35,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -46,31 +58,25 @@ public class JobPortalSecurityConfig {
     @Qualifier("jobseekerPaths")
     private final List<String> jobseekerPaths;
 
-    @Bean
-    SecurityFilterChain customSecurityFilterChain(HttpSecurity http)
-            throws Exception {
+    private final CorsProperties corsProperties;
 
-        return http
-                .csrf(csrfConfig -> csrfConfig
-                        .csrfTokenRepository(
-                                CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .csrfTokenRequestHandler(
-                                new CsrfTokenRequestAttributeHandler()))
-                .cors(corsConfig ->
-                        corsConfig.configurationSource(corsConfigurationSource()))
+    @Bean
+    SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrfConfig -> csrfConfig
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+                .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(requests -> {
                     publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
                     adminPaths.forEach(path -> requests.requestMatchers(path).hasRole("ADMIN"));
                     employerPaths.forEach(path -> requests.requestMatchers(path).hasRole("EMPLOYER"));
+                    jobseekerPaths.forEach(path -> requests.requestMatchers(path).hasRole("JOB_SEEKER"));
                     securedPaths.forEach(path -> requests.requestMatchers(path).authenticated());
                     requests.anyRequest().denyAll();
                 })
-                .addFilterBefore(
-                        new JwtTokenValidatorFilter(publicPaths),
-                        BasicAuthenticationFilter.class)
-                .formLogin(form -> form.disable())
-                .httpBasic(httpBasic -> httpBasic.disable())
-
+                .addFilterBefore(new JwtTokenValidatorFilter(publicPaths), BasicAuthenticationFilter.class)
+                .formLogin(flc -> flc.disable())
+                .httpBasic(hbc -> hbc.disable())
                 .exceptionHandling(exception -> exception
                                 .accessDeniedHandler((request, response, accessDeniedException) -> {
                                     response.setStatus(HttpServletResponse.SC_FORBIDDEN);
@@ -85,40 +91,25 @@ public class JobPortalSecurityConfig {
 
                 )
                 .build();
-
-
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(corsProperties.getAllowedOrigins());
+        config.setAllowedMethods(corsProperties.getAllowedMethods());
+        config.setAllowedHeaders(corsProperties.getAllowedHeaders());
+        config.setAllowCredentials(corsProperties.getAllowCredentials());
+        config.setMaxAge(corsProperties.getMaxAge());
 
-        config.setAllowedOrigins(
-                Arrays.asList("http://localhost:5173"));
-
-        config.setAllowedMethods(
-                Collections.singletonList("*"));
-
-        config.setAllowedHeaders(
-                Collections.singletonList("*"));
-
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return source;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            JobPortalUsernamePwdAuthenticationProvider provider) {
-
-        return new ProviderManager(provider);
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
--- jobportal-backend/src/main/java/com/eazybytes/jobportal/security/util/JwtUtil.java
+++ jobportal-backend/src/main/java/com/eazybytes/jobportal/security/util/JwtUtil.java
@@ -5,6 +5,8 @@ import com.eazybytes.jobportal.entity.JobPortalUser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,30 +15,49 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@PropertySource(value = "classpath:jwt.properties")
 public class JwtUtil {
 
     private final Environment env;
 
+    @Value("${jwt.issuer:Job Portal}")
+    private String jwtIssuer;
+
+    @Value("${jwt.subject:JWT Token}")
+    private String jwtSubject;
+
+    @Value("${jwt.expiration.hours:1}")
+    private int jwtExpirationHours;
+
+    @Value("${jwt.prod.expiration.hours:1}")
+    private int jwtProdExpirationHours;
 
     public String generateJwtToken(Authentication authentication){
         String jwtToken;
-        //String ttlTime = env.getProperty("cache.jobs.ttl-minutes","5");
+        int expirationHours = jwtExpirationHours;
+        // String ttlTime = env.getProperty("cache.jobs.ttl-minutes","5");
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        if (profiles.contains("prod")) {
+            expirationHours = jwtProdExpirationHours;
+        }
         String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
                 ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
         SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         var fetchedUser = (JobPortalUser) authentication.getPrincipal();
-        jwtToken = Jwts.builder().issuer("Job Portal").subject("JWT Token")
+        jwtToken = Jwts.builder().issuer(jwtIssuer).subject(jwtSubject)
                 .claim("name", fetchedUser.getName())
                 .claim("email", fetchedUser.getEmail())
                 .claim("mobileNumber", fetchedUser.getMobileNumber())
                 .claim("roles", authentication.getAuthorities().stream().map(
                         GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
                 .issuedAt(new java.util.Date())
-                .expiration(new java.util.Date((new java.util.Date()).getTime() + 24 * 60 * 60 * 1000))
+                .expiration(new java.util.Date((new java.util.Date()).getTime() + expirationHours * 60 * 60 * 1000))
                 .signWith(secretKey).compact();
         return jwtToken;
     }
